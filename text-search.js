257db2a642de4f52c45ab008fff5a7cfb8bb30bb
Srirangan <srirangan@gmail.com>
2012-08-03 19:01:40 +0530

    Text search example

---
+++ text-search.js
@@ -0,0 +1,17 @@
+var assert = new require("assert");
+
+var config = require("./config.js");
+
+var GooglePlaces = require("googleplaces");
+var googlePlaces = new GooglePlaces(config.apiKey, config.outputFormat);
+var parameters;
+
+/**
+ * Text search - https://developers.google.com/places/documentation/#TextSearchRequests
+ */
+parameters = {
+  query:"restaurants in dublin"
+};
+googlePlaces.textSearch(parameters, function (response) {
+  console.log(response.results);
+});

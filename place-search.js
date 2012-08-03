3d36999fa2eaa0cb052dd47b5cfa35ba5d16ffb5
Srirangan <srirangan@gmail.com>
2012-08-03 19:01:31 +0530

    Place search example

---
+++ place-search.js
@@ -0,0 +1,18 @@
+var assert = new require("assert");
+
+var config = require("./config.js");
+
+var GooglePlaces = require("googleplaces");
+var googlePlaces = new GooglePlaces(config.apiKey, config.outputFormat);
+var parameters;
+
+/**
+ * Place search - https://developers.google.com/places/documentation/#PlaceSearchRequests
+ */
+parameters = {
+  location:[-33.8670522, 151.1957362],
+  types:"doctor"
+};
+googlePlaces.placeSearch(parameters, function (response) {
+  console.log(response.results);
+});

36fda43a356a2b09035a49a0cda759ff5873a4e5
Srirangan <srirangan@gmail.com>
2012-08-03 19:01:49 +0530

    Place details request example

---
+++ place-details-request.js
@@ -0,0 +1,20 @@
+var assert = new require("assert");
+
+var config = require("./config.js");
+
+var GooglePlaces = require("googleplaces");
+var googlePlaces = new GooglePlaces(config.apiKey, config.outputFormat);
+var parameters;
+
+/**
+ * Place details requests - https://developers.google.com/places/documentation/#PlaceDetails
+ */
+parameters = {
+  location:[-33.8670522, 151.1957362],
+  types:"doctor"
+};
+googlePlaces.placeSearch(parameters, function (response) {
+  googlePlaces.placeDetailsRequest({reference:response.results[0].reference}, function (response) {
+    console.log(response.result);
+  });
+});
